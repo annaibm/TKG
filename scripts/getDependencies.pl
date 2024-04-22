4c770e4106adc4209dddca9c9cdf8ad632ecb3e6
Anna Babu Palathingal <148897727+annaibm@users.noreply.github.com>
2024-04-22 13:41:41 -0400

    added the prestage jar

--- scripts/getDependencies.pl
+++ scripts/getDependencies.pl
@@ -286,7 +286,7 @@ my %jars_to_use = $path =~ /system_lib/ ? %system_jars : %base;
 my @jars_info;
 foreach my $dependency (keys %jars_to_use) {
 	foreach my $i (@dependencies) {
-		if ($i eq "all" || exists $jars_to_use{$dependency}) {
+		if ($i eq "all" || e$dependency eq $i) {
 			push(@jars_info, $jars_to_use{$dependency});
 		}
 	}
