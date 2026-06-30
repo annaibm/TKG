a4845d1a5bfa12dc6c2ddc2b6c7328866c4304e5
Anna Babu Palathingal <anna.bp@ibm.com>
2026-06-29 20:24:02 -0400

    Use shell sed instead of Make subst for BUILD_LIST expansion to work rel

--- compile.mk
+++ compile.mk
@@ -29,7 +29,7 @@ include moveDmp.mk
 #######################################
 COMPILE_BUILD_LIST = ${REFINED_BUILD_LIST}
 # Pre-expand BUILD_LIST to avoid charset issues on z/OS
-EXPANDED_BUILD_LIST = TestConfig/build.xml,$(subst ,/build.xml,${COMPILE_BUILD_LIST})/build.xml
+EXPANDED_BUILD_LIST = TestConfig/build.xml,$(shell echo '${COMPILE_BUILD_LIST}' | sed 's/,/\/build.xml,/g')/build.xml
 
 # TEST_FLAG can be empty, Windows ant does not like empty -D<param> values
 ifneq ($(TEST_FLAG),)
